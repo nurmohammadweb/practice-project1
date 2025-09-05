83814752a66e6d28e0c2fcd7b3c92fa37bb341d5
nurmohammadweb <nurmohammad11266@gmail.com>
2025-09-05 18:35:53 +0600

    added done all word

--- script.js
+++ script.js
@@ -4,6 +4,48 @@ const loadLessons = () => {
     .then((json) => displayLesson(json.data));
 };
 
+const loadLevelWord = (id) => {
+  const url =`https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("Word-container");
+  //  console.log(words);
+
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div class="col-span-full bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+     <img class="mx-auto" src="assets/alert-error.png" alt="">
+         <p class="font-semibold ">আপনি এখনো কোন Lesson Select করেন</p>
+        <h2 class="font-bold text-2xl">একটি Lesson Select করুন।</h2>
+        
+       
+      </div>`;
+    return;
+  }
+
+  wordContainer.innerHTML = "";
+  words.forEach((word) => {
+    const clickDiv = document.createElement("div");
+    clickDiv.innerHTML = `<div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+        <h2 class="font-bold text-2xl">${word.word ? word.word : "word পাওয়া যায় নি"}</h2>
+        <p class="font-semibold ">Meaning / Pronuonciation</p>
+        <div class="text-2xl font-medium font-bangla"> ${word.meaning ? word.meaning : "meaning পাওয়া যায় নি"} / ${word.pronuonciation ? word.pronuonciation : "pronuonciation পাওয়া যায় নি"}</div>
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>`;
+
+    wordContainer.append(clickDiv);
+  });
+
+};
+
+
 const displayLesson = (lessons) => {
   // 1.get the container
   
@@ -13,7 +55,7 @@ const displayLesson = (lessons) => {
   for (let lesson of lessons) {
     // 3. create Element
     const btnDiv = document.createElement("div")
-    btnDiv.innerHTML = `<button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open-reader"></i>
+    btnDiv.innerHTML = `<button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open-reader"></i>
               Lesson - ${lesson.level_no}
               </button>`;
     // 4.append into container
